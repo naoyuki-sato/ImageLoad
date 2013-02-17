8a40d9524d81fb9be9b7058b08f5b2be02a92403
Naoyuki Sato <naoyuki.sato@gmail.com>
2013-02-17 21:10:41 +0900

    add comment and fix app name

--- webapp/image.js
+++ webapp/image.js
@@ -5,6 +5,10 @@ console.debug = function() {};
  window.addEventListener("load", function()
  {
     console.log("Image Load Test");
+
+    /*
+     * Set button event
+     */
     var single = document.getElementById("LoadSingle");
     single.onclick = LoadSingleImage;
 
@@ -13,18 +17,22 @@ console.debug = function() {};
 
 });
 
+/*
+ * Load Single image and Set it on image tag
+ */
 var LoadSingleImage = function()
 {
     console.log("Load Simgle Image");
+
     var url = "http://2.media.bustedtees.cvcdn.com/e/-/bustedtees.e8c2409e-884a-4500-9287-ec9ec26933f5.jpg";
 
-    var img = document.getElementById("img").getElementsByTagName("img");
+    var img = document.getElementById("img").getElementsByTagName("img")[0];
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'blob';
 
-    xhr.onload = function(e) {
+    xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var blob = xhr.response;
             img.src = window.URL.createObjectURL(blob);
@@ -34,18 +42,18 @@ var LoadSingleImage = function()
 }
 
 /*
- * This function will be called when "SSDP Start" button is pushed.
+ * Load Multiple images and Set them on image tag
  */
  var LoadMultipleImage = function()
  {
+    console.log("Load Multiple images");
+
     var url = ["http://8.media.bustedtees.cvcdn.com/1/-/bustedtees.21419ad1-ed60-40a0-86ff-823a06226f42.jpg",
     "http://4.media.bustedtees.cvcdn.com/9/-/bustedtees.3cfc1519-e42f-4376-8fbf-980df580fb44.jpg",
     "http://2.media.bustedtees.cvcdn.com/1/-/bustedtees.27977811-360f-4294-9f38-3bd6561d.jpg"];
 
-    // Get Image Div
     var request = new Array(3);
     var list = document.getElementById("imgList").getElementsByTagName("img");
-    console.log(list.length);
 
     for (var i = 0; i < list.length; i++)
     {
@@ -53,7 +61,7 @@ var LoadSingleImage = function()
         request[i].open('GET', url[i], true);
         request[i].responseType = 'blob';
 
-        request[i].onload = (function(x) { return function() {
+        request[i].onreadystatechange = (function(x) { return function() {
             if (request[x].readyState == 4 && request[x].status == 200) {
                 var blob = request[x].response;
                 list[x].src = window.URL.createObjectURL(blob);
